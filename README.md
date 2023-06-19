25fc57c5e3228faba4979dee54c341d396d0314d
Olga Zegarra <olgazegarra@outlook.com>
2023-06-19 12:54:43 +0200

    Add flask

---
+++ README.md
@@ -0,0 +1,10 @@
+# SE-PAGE-SCRAPER
+
+This is a test project to get some commands from streamelements page
+
+## Sample config
+```
+[API]
+api_se_username = user_name
+api_se_endpoint = https://api.streamelements.com/kappa/v2
+```
--- data/kappa.py
+++ data/kappa.py
@@ -4,24 +4,38 @@ import requests
 
 
 class Kappa:
-    def __init__(self, username, url) -> None:
+    def __init__(self) -> None:
         settings = Configuration('config.ini')
 
-        username = settings.get_variable('API', 'api_se_username')
-        if username is not None:
-            print(f'Username: {username}')
+        self.username = settings.get_variable('API', 'api_se_username')
+        if self.username is not None:
+            print(f'Username: {self.username}')
         else:
             print('Username not found in configuration file.')
 
-
-        url = settings.get_variable('API', 'api_se_endpoint')
-        if url is not None:
-            print(f'Endpoint url: {url}')
+        self.url = settings.get_variable('API', 'api_se_endpoint')
+        if self.url is not None:
+            print(f'Endpoint url: {self.url}')
         else:
             print('Endpoint url not found in configuration file.')
 
     def get_commands(self) -> Optional[Dict[str,str]]:
-        endpoint = f"{self.url}{self.username}/commands"
+        user_info = self.get_user_info()
+        user_id = user_info["_id"]
+        endpoint = f"{self.url}/bot/commands/{user_id}/public"
+        print(endpoint)
+        response = requests.get(endpoint)
+        if response.status_code == 200:
+            result = response.json()
+            keys_to_show = ["enabled", "command", "reply"]
+            short_result = [{key: obj[key] for key in keys_to_show} for obj in result]
+            return short_result
+        else:
+            return None
+        
+    def get_user_info(self) -> Optional[Dict[str,str]]:
+        endpoint = f"{self.url}/channels/{self.username}"
+        print(endpoint)
         response = requests.get(endpoint)
         if response.status_code == 200:
             return response.json()
